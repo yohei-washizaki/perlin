7bb1869d1549f3f986899731fede0d220b42cc31
Yohei Washizaki <yohei.washizaki@gmail.com>
2017-05-04 09:26:33 +0900

    Renamed bias.py to scaler.py

---
+++ scaler.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def apply_scale(xy, scale):
+    x = float(xy[0]) * scale
+    y = float(xy[1]) * scale
+    print('{0} {1}'.format(x, y))
+
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+    import util
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("scale", type=int, help="Scale to apply")
+    args = parser.parse_args()
+
+    grid_size = util.power_to_int(args.scale)
+
+    for line in sys.stdin:
+        s = line.strip()
+        xy = s.split(' ')
+        apply_scale(xy, grid_size)
