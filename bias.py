316d50a6d84cb5e8abeb6b9898743a0ed682a4cc
Yohei Washizaki <yohei.washizaki@gmail.com>
2017-05-04 09:07:38 +0900

    Added bias applier.

--- bias.py
+++ bias.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 import argparse
-import fileinput
+import sys
 import util
+
+
+def proc(s, bias):
+    xy = s.split(' ')
+    x = float(xy[0]) * bias
+    y = float(xy[1]) * bias
+    print('{0} {1}'.format(x, y))
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("size", type=int, help="size of a texture. power of 2")
 args = parser.parse_args()
 
-power_of_2_size = util.next_power_of_2(args.size)
-print(power_of_2_size)
+grid_size = util.power_to_int(args.size)
+
+for line in sys.stdin:
+    proc(line.strip(), grid_size)
--- util.py
+++ util.py
@@ -1,6 +1,32 @@
+#!/usr/bin/env python3
 import math
 
 
 def next_power_of_2(x):
 
-    return int(math.pow(2, math.ceil(math.log2(x))))
+    if x == 1:
+        return 1
+
+    if x == 2:
+        return 4
+
+    return int(math.pow(2, math.ceil(math.log2(float(x)))))
+
+
+def power_to_int(x):
+
+    return int(math.pow(x, 2))
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--powerof2", type=int, help="power of 2")
+    parser.add_argument("--power", type=int, help="power")
+    args = parser.parse_args()
+
+    if args.powerof2:
+        print(next_power_of_2(args.powerof2))
+    elif args.power:
+        print(power_to_int(args.power))
