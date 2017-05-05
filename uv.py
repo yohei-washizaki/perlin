585a3365a53e01a41087c771b1dbb28f402e0118
Yohei Washizaki <yohei.washizaki@gmail.com>
2017-05-05 17:17:49 +0900

    Fixed indent.

--- uv.py
+++ uv.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-import argparse
+if __name__ == "__main__":
+    import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("size", type=int, help="size of a texture. power of 2")
-parser.add_argument("-s", "--seed", type=int, help="random seed")
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("size", type=int, help="size of a texture. power of 2")
+    parser.add_argument("-s", "--seed", type=int, help="random seed")
+    args = parser.parse_args()
 
-uvs = []
-inv = 1.0 / args.size
-for h in range(args.size):
-    for w in range(args.size):
-        u = (w + 0.5) * inv
-        v = (h + 0.5) * inv
-        print('{0} {1}'.format(u, v))
+    inv = 1.0 / args.size
+    for h in range(args.size):
+        for w in range(args.size):
+            u = (w + 0.5) * inv
+            v = (h + 0.5) * inv
+            print('{0} {1}'.format(u, v))
