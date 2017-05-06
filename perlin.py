103e225d445e5762b159b6069f78c89a376c9353
Yohei Washizaki <yohei.washizaki@gmail.com>
2017-05-06 18:00:40 +0900

    Added UnitVector class and PerlinNoise class.

--- perlin.py
+++ perlin.py
@@ -2,37 +2,54 @@
 import util
 
 
-def parse_vectors(vectors):
-    v = []
-
-    count = int(len(vectors) / 2)
-    for i in range(count):
-        v.append((vectors[i * 2], vectors[(i * 2) + 1]))
-
-    return v
-
-
-def perlin(aa, ab, ba, bb, x, y, unit_vectors):
-    aav = unit_vectors[aa]
-    abv = unit_vectors[ab]
-    bav = unit_vectors[ba]
-    bbv = unit_vectors[bb]
-    aap = (x, y)
-    abp = (x, y - 1)
-    bap = (x - 1, y)
-    bbp = (x - 1, y - 1)
-    return util.lerp(
-        util.lerp(grad(aav, aap), grad(bav, bap), util.fade(x)),
-        util.lerp(grad(abv, abp), grad(bbv, bbp), util.fade(x)),
-        util.fade(y))
-
-
 def grad(grad_vector, position_vector):
     return util.dot_vector(grad_vector, position_vector)
 
 
-def bind(v):
-    return (v + 1.0) * 0.5
+def bind(v, min_value, max_value):
+    scalar = max_value - min_value
+    half = scalar * 0.5
+    return (v + scalar) * half
+
+
+class Perlin:
+    """Generate perlin noise"""
+
+    def generate_noise(self, aa, ab, ba, bb, x, y, unit_vectors):
+        aav = unit_vectors[aa]
+        abv = unit_vectors[ab]
+        bav = unit_vectors[ba]
+        bbv = unit_vectors[bb]
+        aap = (x, y)
+        abp = (x, y - 1)
+        bap = (x - 1, y)
+        bbp = (x - 1, y - 1)
+
+        noise = util.lerp(
+            util.lerp(grad(aav, aap), grad(bav, bap), util.fade(x)),
+            util.lerp(grad(abv, abp), grad(bbv, bbp), util.fade(x)),
+            util.fade(y))
+
+        return bind(noise, 0, 1)
+
+
+class UnitVectors:
+    """List of unit vectors"""
+
+    def __init__(self, vectors):
+        self.vectors = self.parse_vectors(vectors)
+
+    def parse_vectors(self, vectors):
+        v = []
+
+        count = int(len(vectors) / 2)
+        for i in range(count):
+            v.append((vectors[i * 2], vectors[(i * 2) + 1]))
+
+        return v
+
+    def __getitem__(self, k):
+        return self.vectors[k % len(self.vectors)]
 
 
 if __name__ == "__main__":
@@ -45,16 +62,17 @@ if __name__ == "__main__":
                         help="whitespace separated unit vectors list")
     args = parser.parse_args()
 
-    unit_vectors = parse_vectors(args.unitvectors)
+    unit_vectors = UnitVectors(args.unitvectors)
+    perlin = Perlin()
 
     for line in sys.stdin:
         params = line.strip().split(' ')
-        aa = int(params[0]) & 0x7
-        ab = int(params[1]) & 0x7
-        ba = int(params[2]) & 0x7
-        bb = int(params[3]) & 0x7
+        aa = int(params[0])
+        ab = int(params[1])
+        ba = int(params[2])
+        bb = int(params[3])
         x = float(params[4])
         y = float(params[5])
 
-        p = bind(perlin(aa, ab, ba, bb, x, y, unit_vectors))
+        p = perlin.generate_noise(aa, ab, ba, bb, x, y, unit_vectors)
         print('{0}'.format(p))
